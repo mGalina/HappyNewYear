76accc35ddc99f89f0073c202030b06e013ab0af
Galina <g.marakina.2gis@gmail.com>
2021-06-30 19:03:14 +0300

    Add switch

--- app/src/main/java/com/example/happy_new_year/MainActivity.java
+++ app/src/main/java/com/example/happy_new_year/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.happy_new_year;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private CardView news1Card, news2Card, news3Card, news4Card, news5Card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        news1Card = (CardView) findViewById(R.id.card_1);
+        news2Card = (CardView) findViewById(R.id.card_2);
+        news3Card = (CardView) findViewById(R.id.card_3);
+        news4Card = (CardView) findViewById(R.id.card_4);
+        news5Card = (CardView) findViewById(R.id.card_5);
+
+        news1Card.setOnClickListener(this);
+        news2Card.setOnClickListener(this);
+        news3Card.setOnClickListener(this);
+        news4Card.setOnClickListener(this);
+        news5Card.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i;
+
+        switch (view.getId()){
+            case (R.id.card_1):
+                i = new Intent(this, MainActivity1.class);
+                startActivity(i);
+                break;
+            case (R.id.card_2):
+                i = new Intent(this, MainActivity2.class);
+                startActivity(i);
+                break;
+            case (R.id.card_3):
+                i = new Intent(this, MainActivity3.class);
+                startActivity(i);
+                break;
+            case (R.id.card_4):
+                i = new Intent(this, MainActivity4.class);
+                startActivity(i);
+                break;
+            case (R.id.card_5):
+                i = new Intent(this, MainActivity5.class);
+                startActivity(i);
+                break;
+            default: break;
+        }
     }
 }
