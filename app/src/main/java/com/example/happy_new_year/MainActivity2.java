f96235015a4a0b6e1c133591aba878b30a0dce9d
Galina <g.marakina.2gis@gmail.com>
2021-06-30 19:10:39 +0300

    Add activity

---
+++ app/src/main/java/com/example/happy_new_year/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.happy_new_year;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView text = (TextView) findViewById(R.id.text_2);
+    }
+}
---
+++ app/src/main/java/com/example/happy_new_year/MainActivity5.java
@@ -0,0 +1,17 @@
+package com.example.happy_new_year;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+        TextView text = (TextView) findViewById(R.id.text_5);
+    }
+}
